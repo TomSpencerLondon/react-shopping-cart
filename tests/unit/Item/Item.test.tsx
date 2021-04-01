dd367bfcb5edbd5340d521a6213e6bca2544a6a0
Tom <thomas.spencer@codurance.com>
2021-04-01 11:30:00 +0100

    add code for testing library on user event

--- tests/unit/Item/Item.test.tsx
+++ tests/unit/Item/Item.test.tsx
@@ -3,7 +3,10 @@ import Item from '../../../src/Item/Item';
 import {render, screen } from "@testing-library/react";
 import {CartItemType} from "../../../src/App";
 
-// extend to check for other things
+// 1. extend to check for other things
+// 2. react testing library user event click
+// https://testing-library.com/docs/ecosystem-user-event/
+
 describe('it works', () => {
   it('works', () => {
     // arrange
@@ -17,3 +20,21 @@ describe('it works', () => {
     screen.getByText("Title");
   })
 })
+
+// Possible code snippet from: https://testing-library.com/docs/ecosystem-user-event/
+// example:
+// import React from 'react'
+// import { render, screen } from '@testing-library/react'
+// import userEvent from '@testing-library/user-event'
+//
+// test('click', () => {
+//   render(
+//     <div>
+//       <label htmlFor="checkbox">Check</label>
+//       <input id="checkbox" type="checkbox" />
+//     </div>
+//   )
+//
+//   userEvent.click(screen.getByText('Check'))
+//   expect(screen.getByLabelText('Check')).toBeChecked()
+// })

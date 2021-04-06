68b600456fb764b84e962ac274fdba90c2fa2cde
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2021-04-06 09:18:43 +0100

    Add test for remove from cart

--- tests/unit/CartItem/CartItem.test.tsx
+++ tests/unit/CartItem/CartItem.test.tsx
@@ -18,4 +18,32 @@ describe('CartItem', () => {
     // assert
     expect(screen.getByText("Title")).toBeInTheDocument();
   });
+
+  it('can add to the cart', () => {
+    // arrange
+    const item: CartItemType = {amount: 0, category: "", description: "", id: 0, image: "", price: 0, title: "Title"};
+    const addToCart = jest.fn();
+    const removeFromCart = jest.fn();
+    render(<CartItem item={item} addToCart={addToCart} removeFromCart={removeFromCart} />)
+
+    // act
+    userEvent.click(screen.getByText('+'))
+
+    // assert
+    expect(addToCart).toBeCalledWith(item);
+  });
+
+  it('can remove from the cart', () => {
+    // arrange
+    const item: CartItemType = {amount: 0, category: "", description: "", id: 0, image: "", price: 0, title: "Title"};
+    const addToCart = jest.fn();
+    const removeFromCart = jest.fn();
+    render(<CartItem item={item} addToCart={addToCart} removeFromCart={removeFromCart} />)
+
+    // act
+    userEvent.click(screen.getByText('-'))
+
+    // assert
+    expect(removeFromCart).toBeCalledWith(item.id);
+  });
 });

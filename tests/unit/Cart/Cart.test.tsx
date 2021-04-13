03cf9b28a97dc5d569f2438a6b5b51fbd18a3be6
Tom <thomas.spencer@codurance.com>
2021-04-13 08:25:57 +0100

    add passing test for the price

--- tests/unit/Cart/Cart.test.tsx
+++ tests/unit/Cart/Cart.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Cart from "../../../src/Cart/Cart";
 import * as CartItem from "../../../src/CartItem/CartItem";
 import {CartItemType} from "../../../src/App";
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import Item from "../../../src/Item/Item";
 
 describe('Cart', () => {
@@ -21,6 +21,23 @@ describe('Cart', () => {
 
     // assert
     cartItems.forEach((item) => expect(spyOnCartItem).toBeCalledWith({ item, addToCart, removeFromCart }, {} ));
+  });
+
+  it('renders the correct amount', () => {
+    // arrange
+    const item1: CartItemType = {amount: 1, category: "", description: "", id: 0, image: "", price: 5.0, title: "Item1"};
+    const item2: CartItemType = {amount: 2, category: "", description: "", id: 1, image: "", price: 10.0, title: "Item2"};
+    const addToCart = jest.fn();
+    const removeFromCart = jest.fn();
+    const cartItems: CartItemType[] = [item1, item2];
+    const spyOnCartItem = jest.spyOn(CartItem, "default");
+
+    // act
+    render(<Cart cartItems={cartItems} addToCart={addToCart} removeFromCart={removeFromCart} />)
+
+
+    // assert
+    expect(screen.getByText('Total: $25.00')).toBeInTheDocument();
   });
 });
 

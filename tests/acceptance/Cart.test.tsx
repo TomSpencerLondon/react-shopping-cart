6e26cd3478ecf47c49a6619e017458dfda28808a
Tom <thomas.spencer@codurance.com>
2021-04-29 04:59:39 +0100

    add code for cart test

--- tests/acceptance/Cart.test.tsx
+++ tests/acceptance/Cart.test.tsx
@@ -2,18 +2,21 @@ import * as React from 'react';
 import {CartItem} from "../../src/CartItem/data/CartItem";
 import {createFakeCartItem} from "../helpers/factories/CartItem";
 import * as itemApi from '../../src/Item/api/getItems';
-import {render, screen} from "@testing-library/react";
+import {render, screen, waitFor} from "@testing-library/react";
 import App from "../../src/App";
 import userEvent from "@testing-library/user-event";
-jest.mock('react-query', () => ({ useQuery: () => ({ isLoading: false, error: {}, data: [], }), }));
+const item1: CartItem = createFakeCartItem();
+const item2: CartItem = createFakeCartItem();
+const item3: CartItem = createFakeCartItem();
+
+jest.mock('react-query', () => ({ useQuery: () => ({ isLoading: false, data: [item1, item2, item3], }), }));
+
 
 test('should be able to handle adding and removing items and getting the total', () => {
   // arrange
   // create items
   // create cart
-  const item1: CartItem = createFakeCartItem();
-  const item2: CartItem = createFakeCartItem();
-  const item3: CartItem = createFakeCartItem();
+
 
   jest.spyOn(itemApi, "getItems").mockResolvedValue([item1, item2, item3]);
 
@@ -22,7 +25,7 @@ test('should be able to handle adding and removing items and getting the total',
   // add 2 items to the cart
   // remove 1 item
   render(<App />)
-  userEvent.click(screen.getByText('+'))
+  userEvent.click(screen.getByText('Add to cart'))
 
   // assert
   // the items expect in cart are in the cart - and the total is correct

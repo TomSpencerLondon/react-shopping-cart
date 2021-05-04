d8427ed17223e163f5175d915f357b50e8e0c7e9
Tom <thomas.spencer@codurance.com>
2021-05-04 09:06:13 +0100

    add passing test for acceptance

--- tests/acceptance/Cart.test.tsx
+++ tests/acceptance/Cart.test.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import {CartItem} from "../../src/CartItem/data/CartItem";
 import {createFakeCartItem} from "../helpers/factories/CartItem";
 import * as itemApi from '../../src/Item/api/getItems';
-import {render, screen} from "../helpers/test-utils";
+import {render, renderAct, screen} from "../helpers/utils/test-utils";
 import App from "../../src/App";
 import userEvent from "@testing-library/user-event";
+import { waitFor } from "@testing-library/react";
 
-test('should be able to handle adding and removing items and getting the total', () => {
+test('should be able to handle adding and removing items and getting the total', async () => {
   // arrange
   // create items
   // create cart
@@ -21,10 +22,12 @@ test('should be able to handle adding and removing items and getting the total',
   // render the App
   // add 2 items to the cart
   // remove 1 item
-  render(<App />)
-  userEvent.click(screen.getByTestId('add-to-cart-button1'))
-  userEvent.click(screen.getByTestId('add-to-cart-button2'))
+  await renderAct(<App />);
 
+  await waitFor(() => {
+    userEvent.click(screen.getByTestId('add-to-cart-button1'))
+    userEvent.click(screen.getByTestId('add-to-cart-button2'))
+  });
   // assert
   // the items expect in cart are in the cart - and the total is correct
   // expect(screen.getByText(/logout/i)).toBeInTheDocument()
--- tests/helpers/utils/test-utils.tsx
+++ tests/helpers/utils/test-utils.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import {act, render} from '@testing-library/react';
 import { QueryClientProvider } from 'react-query';
-import { queryClient } from '../../src/App';
-// import { ThemeProvider } from "my-ui-lib"
-// import { TranslationProvider } from "my-i18n-lib"
-// import defaultStrings from "i18n/en-x-default"
-// const Providers = ({ children }: any) => (
-// <ThemeProvider theme="light">
-//   <TranslationProvider messages={defaultStrings}>
-//     {children}
-//   </TranslationProvider>
-// </ThemeProvider>
-// );
+import { queryClient } from '../../../src/App';
+
+export const renderAct = async (Component: ReactNode) => {
+  await act(async () => {
+    render(<QueryClientProvider client={queryClient}>{Component}</QueryClientProvider>);
+  });
+};
+
 export const wrapperWithQueryClient = ({ children }: any) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 );

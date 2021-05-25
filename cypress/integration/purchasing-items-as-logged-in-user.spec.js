6ea10df2ce6f514f6d534bf2c33855f4dc71ffd1
Tom <thomas.spencer@codurance.com>
2021-05-25 08:44:20 +0100

    add test for logging in

--- cypress/integration/purchasing-items-as-logged-in-user.spec.js
+++ cypress/integration/purchasing-items-as-logged-in-user.spec.js
@@ -4,10 +4,9 @@ context("findArtistAndVisitTheirPage", () => {
   });
 
   it("should have title text", () => {
-    cy.get("button").contains("ADD TO CART").click();
-    // cy.url().should("include", "/artists/5K4W6rqBFWDnAN6FQUkS6x");
-    // cy.contains("Kanye West");
-    // cy.contains("rap");
-    // cy.contains("Gold Digger");
+    cy.get("[data-testid='add-to-cart-button5']").first().click();
+    cy.get("button").contains("1").click();
+    cy.get("button").contains("login").click();
+    cy.contains("Is logged in");
   });
 });
